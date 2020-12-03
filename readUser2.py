090783823c65f50e026c83295348161b50d15242
Nguyen Manh Cuong <cuonga1k57@gmail.com>
2020-12-03 20:52:43 +0700

    ADD readUser2

--- readUser2.py
+++ readUser2.py
@@ -2,7 +2,7 @@ import rule
 import notes
 
 def search(readuser2, readuser1):
-    result = rule.list_result(readuser1)
+    result = readuser1
     for i in readuser2:
         result = notes.criteria(i, result)
     
